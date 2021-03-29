24684ebdc452ab30d76f51927d984055f292db0d
Beca <arj2763@gmail.com>
2021-03-29 16:35:57 +0800

    feat: delete default Vue files, add login page, fix Vuetify unknown custom element console error

--- chat-room/src/plugins/vuetify.js
+++ chat-room/src/plugins/vuetify.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
-import Vuetify from "vuetify/lib/framework";
-
+import Vuetify from "vuetify";
+// default會出現Unknown custom element
+// import Vuetify from 'vuetify/lib/framework'
 Vue.use(Vuetify);
 
 export default new Vuetify({});
