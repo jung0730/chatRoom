9ee33c1b38c059d8b0e2b808fc78fd9be50999b2
jung <arj2763@gmail.com>
2021-05-09 21:12:01 +0800

    fix: add custom headers

--- chat-room/vue.config.js
+++ chat-room/vue.config.js
@@ -6,7 +6,8 @@ module.exports = {
         target: 'http://104.214.48.227:8080/api/',
         pathRewrite: { '^/api': ''},
         changeOrigin: true,
-        ws: true
+        ws: true,
+        headers: { 'X-Request-UID': 'fe4b814c-0c4f-44d5-b3e5-c6a38cc0cba6'}
       }
     }
   }
